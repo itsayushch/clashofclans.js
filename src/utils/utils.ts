2e161182a64e4f6eaa76bbd44e6d85a4a0da6a93
SUVAJIT <suvajit.me@gmail.com>
2020-11-25 17:34:06 +0530

    Season Info Method
    Season End and Start Date
    Season ID

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -28,3 +28,15 @@ export async function fetchURL(url: string, token: string, timeout: number) {
 		ok: res.status === 200
 	});
 }
+
+export function seasonEnd(month: number, year = new Date().getFullYear()) {
+	let day = 0;
+	let lastDate = new Date(Date.UTC(year, month));
+	while (true) {
+		lastDate = new Date(Date.UTC(year, month, day, 5, 0));
+		if (lastDate.getDay() === 1) break;
+		day -= 1;
+	}
+
+	return lastDate;
+}
