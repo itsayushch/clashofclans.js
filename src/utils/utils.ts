489d8b7a2785a683a0d89658027de7d3bc6cc78c
SUVAJIT <suvajit.me@gmail.com>
2020-11-25 16:21:56 +0530

    cleanup

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
-export function validateTag(tag: string, encode = false): string | false {
-	if (!tag) return false;
-	const _tag = tag.toUpperCase().replace(/O/g, '0').replace('#', '');
+export function validateTag(msg: string, encode = false): string | false {
+	if (!msg) return false;
+	const tag = msg.toUpperCase().replace(/O/g, '0').replace('#', '');
 	const tagRegex = /[0289PYLQGRJCUV]{3,9}/g;
-	const result = tagRegex.exec(_tag);
+	const result = tagRegex.exec(tag);
 	return result ? encode ? encodeURIComponent(`#${result[0]}`) : `#${result[0]}` : false;
 }
 
